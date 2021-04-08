acf25cacee877bd5a3cabc27745aeb41bd960c3f
Peyton Mader <middpeyton@gmail.com>
2021-04-08 11:36:29 -0700

    complete challenges on js file

--- functions.js
+++ functions.js
@@ -3,31 +3,46 @@
 //Challenge 1
 // put this code block into a function, add a return statement, and call the function
 
-
-let bestPet = 'dog';
-let petName = 'indiana'
-let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
-// return the petString here
+function dogName(){
+  let bestPet = 'dog';
+  let petName = 'indiana'
+  let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
+  // return the petString here
+  return petString;
+}
 
 // call your function here
-
+dogName();
 
 
 //Challenge 2
 // put this code block into a function, add a return statement, and call the function
 
-let anotherGreatPet = 'bunny';
-let anotherPetName = 'opal';
-let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
-// return the anotherPetString here
+function bunnyName(){
+  let anotherGreatPet = 'bunny';
+  let anotherPetName = 'opal';
+  let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+  // return the anotherPetString here
+  return anotherPetString;
+}
 
 // call your function here
-
+bunnyName();
 
 
 // Challenge 3
 // is there another way to write this function so we can use one function for different pets?
 
+petFinal = prompt('What type of animal is your pet?')
+nameFinal = prompt('What is your pet\'s name?')
+
+
+function animalNames(petFinal, nameFinal){
+let petAnnouncement = 'The best pet is a ' + petFinal + ' named ' + nameFinal;
+return petAnnouncement;
+}
+
+console.log(petAnnouncement);
 
 
 
