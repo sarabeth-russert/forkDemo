bf1c0dcf6bccde21549f327d38c3d41a9829c621
Troy Russell Tyrrell III <troy.r.tyrrell@gmail.com>
2021-04-08 11:38:08 -0700

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,35 +1,9 @@
-'use strict';
-
-//Challenge 1
-// put this code block into a function, add a return statement, and call the function
-
-function bestPets() {
-  let bestPet = 'dog';
-  let petName = 'indiana'
+function hiSarah() {
+  let bestPet = 'Blue Nose Pitbull';
+  let petName = 'Felony'
   let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
   // return the petString here
   return petString;
 }
 // call your function here
 bestPets();
-
-
-//Challenge 2
-// put this code block into a function, add a return statement, and call the function
-
-let anotherGreatPet = 'bunny';
-let anotherPetName = 'opal';
-let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
-// return the anotherPetString here
-
-// call your function here
-
-
-
-// Challenge 3
-// is there another way to write this function so we can use one function for different pets?
-
-
-
-
-
