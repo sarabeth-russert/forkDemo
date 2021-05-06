b0567d1a87de2b895d95f513192fce16bb9ee721
REVATHI MYLAVARAPU <revathisrie@gmail.com>
2021-05-06 11:26:15 -0700

    completed function challenge #2 and challenge #3

--- functions.js
+++ functions.js
@@ -17,20 +17,28 @@ bestPetsFunction();
 
 //Challenge 2
 // put this code block into a function, add a return statement, and call the function
-
+function petFunction(){
 let anotherGreatPet = 'bunny';
 let anotherPetName = 'opal';
 let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
 // return the anotherPetString here
+return anotherPetString;
+}
 
 // call your function here
-
+petFunction();
 
 
 // Challenge 3
 // is there another way to write this function so we can use one function for different pets?
+function diffPets(name1,name2){
+let petName1 = name1;
+let petName2 = name2;
 
+let petNames = 'The best pets are ' +petName1+ ' and ' + petName2;
+return petNames;
 
-
-
+}
+function diffPets(Kittu, Sweety)
+  
 
