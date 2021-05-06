4e63ba2675279563a81ee579ab45833281fb1d48
Benjamin Ibarra <loco.queens@gmail.com>
2021-05-06 11:39:45 -0700

    commiting changes for forkDemo assignment

--- functions.js
+++ functions.js
@@ -17,19 +17,31 @@ bestPetsFunction();
 
 //Challenge 2
 // put this code block into a function, add a return statement, and call the function
-
-let anotherGreatPet = 'bunny';
-let anotherPetName = 'opal';
-let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
-// return the anotherPetString here
+function petGroup(){
+  let anotherGreatPet = 'bunny';
+  let anotherPetName = 'opal';
+  let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+  // return the anotherPetString here
+  return anotherPetString
+}
 
 // call your function here
-
+petGroup();
 
 
 // Challenge 3
 // is there another way to write this function so we can use one function for different pets?
 
+function petGroup(anotherPetName, anotherGreatPet){
+  let anotherGreatPet = 'bunny';
+  let anotherPetName = 'opal';
+  let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+  // return the anotherPetString here
+  return anotherPetString;
+}
+
+// call your function here
+petGroup();
 
 
 
