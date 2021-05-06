34e0395a1c139403d4cff042375dbd382e24ed1c
Joshua Haddock <joshhaddock88@gmail.com>
2021-05-06 11:28:36 -0700

    Complete challenges

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@
 //Challenge 1
 // put this code block into a function, add a return statement, and call the function
 
-function bestPetsFunction() {
+function bestPets() {
   let bestPet = 'dog';
   let petName = 'indiana'
   let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
@@ -12,25 +12,33 @@ function bestPetsFunction() {
 }
 
 // call your function here
-bestPetsFunction();
+bestPets();
 
 
 //Challenge 2
 // put this code block into a function, add a return statement, and call the function
+function otherPets() {
+  let anotherGreatPet = 'bunny';
+  let anotherPetName = 'opal';
+  let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
 
-let anotherGreatPet = 'bunny';
-let anotherPetName = 'opal';
-let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+
 // return the anotherPetString here
-
+return anotherPetString;
+}
 // call your function here
-
+otherPets();
 
 
 // Challenge 3
 // is there another way to write this function so we can use one function for different pets?
+function allGoodPets(petIndex) {
+  let petType = ['dog', 'bunny'];
+  let petName = ['indiana', 'opal'];
+  let petString = 'The best pet is a ' + petType[petIndex] + ' named ' + petName[petIndex];
+  return petString
+}
 
-
-
+allGoodPets();
 
 
