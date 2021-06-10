09d046a4a50ef56a2e01b4566d67a738b960be02
jesse dills <jessedills@yahoo.com>
2021-06-10 15:01:01 -0400

    made functions

--- functions.js
+++ functions.js
@@ -18,20 +18,25 @@ funFunction();
 
 // -------- Challenge 2 -------- //
 // put this code block into a function, add a return statement, and call the function
-
-let anotherGreatPet = 'bunny';
-let anotherPetName = 'opal';
-let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+function greatPet() {
+    let anotherGreatPet = 'bunny';
+    let anotherPetName = 'opal';
+    let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+    return anotherPetString;
+}
 // return the anotherPetString here
 
 // call your function here
-
+greatPet();
 
 
 // -------- Challenge 3 -------- //
 // is there another way to write this function so we can use one function for different pets?
 
-
+function allPets(dog, Chester) {
+    let petStringAgain = 'The best pet is a ' + dog + ' named ' + Chester;
+    return petStringAgain;
+}
 
 
 
