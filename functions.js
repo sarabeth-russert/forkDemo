509d25b2a20ed4b3ae353e58bca93eec2c245384
Joe Ivans <joseph.ivans@gmail.com>
2021-07-15 11:36:48 -0700

    Add challenge impl

--- functions.js
+++ functions.js
@@ -20,18 +20,30 @@ bestPet()
 // -------- Challenge 2 -------- //
 // put this code block into a function, add a return statement, and call the function
 
-let anotherGreatPet = 'bunny';
-let anotherPetName = 'opal';
-let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
 // return the anotherPetString here
+function getPetName() {
+  let anotherGreatPet = 'bunny';
+  let anotherPetName = 'opal';
+  let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+
+  return anotherPetString;
+}
 
 // call your function here
+getPetName();
 
 
 
 // -------- Challenge 3 -------- //
 // is there another way to write this function so we can use one function for different pets?
 
+// return the anotherPetString here
+function getPetNameDynamic(petType, petName) {
+  return 'The best pet is a ' + petType + ' named ' + petName;
+}
+
+// call your function here
+getPetNameDynamic('dog', 'Clint Eastwood');
 
 
 
