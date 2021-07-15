657f8369310ce5b6ee4ab7c73ea6795f88e666eb
kason <kbraley9@gmail.com>
2021-07-15 11:34:29 -0700

    Add functions

--- functions.js
+++ functions.js
@@ -1,38 +1,41 @@
-'use strict';
+"use strict";
 
 // -------- Challenge 1 -------- //
 // put this code block into a function, add a return statement, and call the function
 
-
 // return the petString here
-function bestPet(){
-  let bestPet = 'dog';
-  let petName = 'indiana'
-  let petString = 'The best pet is a ' + bestPet + ' named ' + petName;
+function bestPet() {
+  let bestPet = "dog";
+  let petName = "indiana";
+  let petString = "The best pet is a " + bestPet + " named " + petName;
   return petString;
 }
 
-
 // call your function here
-bestPet()
-
+bestPet();
 
 // -------- Challenge 2 -------- //
 // put this code block into a function, add a return statement, and call the function
 
-let anotherGreatPet = 'bunny';
-let anotherPetName = 'opal';
-let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
-// return the anotherPetString here
-
+function bestPet() {
+  let anotherGreatPet = "bunny";
+  let anotherPetName = "opal";
+  let anotherPetString =
+    "The best pet is a " + anotherGreatPet + " named " + anotherPetName;
+  // return the anotherPetString here
+  return anotherPetString;
+}
 // call your function here
-
-
+bestPet();
 
 // -------- Challenge 3 -------- //
 // is there another way to write this function so we can use one function for different pets?
 
+function amazingPet(pet, name) {
+  let anotherPetString = "The best pet is a " + pet + " named " + name;
+  return anotherPetString;
+}
 
-
-
-
+console.log(amazingPet("dog", "Mango"));
+console.log(amazingPet("dog", "Ollie"));
+console.log(amazingPet("cat", "Pumpkin"));
