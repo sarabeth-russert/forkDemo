74d99b234d317e1b9154376204b09d3976c195c5
raajv <raaj.vardhan@gmail.com>
2021-07-16 00:13:42 +0530

    change to the js added functions

--- functions.js
+++ functions.js
@@ -19,10 +19,13 @@ bestPet()
 
 // -------- Challenge 2 -------- //
 // put this code block into a function, add a return statement, and call the function
-
+function bestestPet(){
 let anotherGreatPet = 'bunny';
 let anotherPetName = 'opal';
 let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anotherPetName;
+return anotherPetString;
+}
+bestestPet()
 // return the anotherPetString here
 
 // call your function here
@@ -31,7 +34,12 @@ let anotherPetString = 'The best pet is a ' + anotherGreatPet + ' named ' + anot
 
 // -------- Challenge 3 -------- //
 // is there another way to write this function so we can use one function for different pets?
-
+function awesomePet(a,b){
+  
+  return returnPet ='the best pet is a ' + a + 'named' + b; 
+}
+awesomePet(DOG , iNDIA)
+console.log(returnPet)
 
 
 
